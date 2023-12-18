1dfa1e08874ef6c6cb43ce7398e8effe503e3753
vimal <vimalpappu999@gmail.com>
2023-12-18 21:07:12 +0530

    serializers

--- django_rest/firstapp/urls.py
+++ django_rest/firstapp/urls.py
@@ -1,6 +1,8 @@
+from django import views
 from django.urls import path
+from .views import student_details
 # from .views import *
 urlpatterns = [
-    # path('', Home.as_view(), name='home')
+    path('students/',views.student_details, name='student'),
     # path('register',UserRegisterView.as_view()),
 ]
--- django_rest/firstapp/views.py
+++ django_rest/firstapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from .models import Student
+from .seriallizers import Studetserializer
+from rest_framework import jSONRenderer
+from django.http import HttpResponse
 # Create your views here.
+
+def student_details(request):
+    stu = Student.objects.get(roll = 2)
+    seraliser = Studetserializer(stu)
+    json_data = jSONRenderer().render(seraliser.data)
+    return HttpResponse(json_data,content_type ="application/json")
